4f17ca7f0158378ff85b399ed5d015c9da1fe443
sameli <sohrab5888@gmail.com>
2016-07-03 10:31:19 -0300

    Update: edit activity

--- app/src/main/java/com/expensedroid/expensedroid/EditActivity.java
+++ app/src/main/java/com/expensedroid/expensedroid/EditActivity.java
@@ -1,7 +1,27 @@
 package com.expensedroid.expensedroid;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 /**
  * Created by S. Ameli on 03/07/16.
  */
-public class EditActivity {
+public class EditActivity extends AppCompatActivity {
+
+    private int database_id = -1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+
+
+    public void btnDiscard(View view) {
+        Intent mainIntent = new Intent(this, MainActivity.class);
+        startActivity(mainIntent); // this will switch to DetailActivity
+    }
+
 }
