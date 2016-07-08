1662062f31f828f14c0077345f9f15eebc69342d
sameli <sohrab5888@gmail.com>
2016-07-08 11:05:30 -0300

    Add: SettingsIO class

---
+++ app/src/main/java/com/expensedroid/expensedroid/SettingsIO.java
@@ -0,0 +1,28 @@
+package com.expensedroid.expensedroid;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by S. Ameli on 08/07/16.
+ */
+public class SettingsIO {
+
+    public static final String SETTINGS_TITLE = "settings";
+
+    public static <T> T readData(Context context, T data, String key){
+        SharedPreferences settings = context.getSharedPreferences(SETTINGS_TITLE, 0);
+
+        if(data instanceof String){
+            return (T) (String) settings.getString(key,data);
+
+        }else if(data instanceof Integer){
+            return (T) (Integer) settings.getInt(key, data);
+
+        }else if(data instanceof Boolean){
+            return (T) (Boolean) settings.getBoolean(key, data);
+        }
+
+        return null;
+    }
+}
