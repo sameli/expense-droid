5329c22e9c87645eecb406db3767f4af1458ae2b
sameli <sohrab5888@gmail.com>
2016-07-13 11:08:51 -0300

    Add: dialog filter listener

---
+++ app/src/main/java/com/expensedroid/expensedroid/DialogFilterDateListener.java
@@ -0,0 +1,8 @@
+package com.expensedroid.expensedroid;
+
+/**
+ * Created by S. Ameli on 12/07/16.
+ */
+public interface DialogFilterDateListener {
+    void onApplyFilterBtn(String selectedEquality, String selectedDate);
+}
