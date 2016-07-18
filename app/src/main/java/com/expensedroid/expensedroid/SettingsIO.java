d38673acca4300cf828599f1b0c7a2f9ff530229
sameli <sohrab5888@gmail.com>
2016-07-18 08:52:57 -0300

    Fix: settingsIO

--- app/src/main/java/com/expensedroid/expensedroid/SettingsIO.java
+++ app/src/main/java/com/expensedroid/expensedroid/SettingsIO.java
@@ -10,6 +10,7 @@ public class SettingsIO {
 
     public static final String SETTINGS_TITLE = "settings";
 
+
     public static <T> T readData(Context context, T data, String key){
         SharedPreferences settings = context.getSharedPreferences(SETTINGS_TITLE, 0);
 
@@ -26,16 +27,30 @@ public class SettingsIO {
         return null;
     }
 
+    /*
+    when passing data to this method, preferably declare type
+     */
     public static <T> Boolean saveData(Context context, T data, String key) {
         SharedPreferences settings = context.getSharedPreferences(SETTINGS_TITLE, 0);
         SharedPreferences.Editor editor = settings.edit();
 
         if (data instanceof String) {
-            System.out.println("saveData: data is String");
+            //System.out.println("saveData: data is String");
             editor.putString(key, (String) data);
+            editor.commit();
+
+        } else if (data instanceof Integer) {
+            //System.out.println("saveData: data is Integer");
+            editor.putInt(key, (Integer) data);
+            editor.commit();
+
+        } else if (data instanceof Boolean) {
+            //System.out.println("saveData: data is Boolean");
+            editor.putBoolean(key, (Boolean) data);
             editor.commit();
         }
 
         return null;
     }
+
 }
